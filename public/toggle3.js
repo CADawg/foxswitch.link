894f716b35e9f87adad26c9636decf812c367f27
Snaddyvitch-Dispenser <28988626+Snaddyvitch-Dispenser@users.noreply.github.com>
2020-09-02 21:27:00 +0100

    Attempt Fix

---
+++ public/toggle3.js
@@ -0,0 +1,34 @@
+let justPosted = false;
+let lastOptions = false;
+
+window.addEventListener("load", function () {
+    document.getElementById("checkbox").addEventListener("change", function () {if (document.getElementById("checkbox").checked !== lastOptions) {justPosted = true; axios.post('/', {on: document.getElementById("checkbox").checked})}});
+});
+
+axios.options("/").then(function (response) {
+    if (justPosted) {
+        justPosted = false;
+        return;
+    }
+
+    let o = (response.data.name || "").includes("on");
+    lastOptions = o;
+    document.getElementById("checkbox").checked = o;
+}).catch(function () {
+    console.log("Failed to get current state!");
+});
+
+setInterval(async function () {
+    axios.options("/").then(function (response) {
+        if (justPosted) {
+            justPosted = false;
+            return;
+        }
+
+        let o = (response.data.name || "").includes("on");
+        lastOptions = o;
+        document.getElementById("checkbox").checked = o;
+    }).catch(function () {
+        console.log("Failed to get current state!");
+    });
+}, 1000);
