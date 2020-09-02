c0e64230ff7ba2bb47fedd535a74c5b3405d2a83
Rishi Panthee <rrpanthe@ncsu.edu>
2020-09-02 13:46:14 -0500

    Discord is done

--- src/discord.js
+++ src/discord.js
@@ -1,15 +1,11 @@
+require('dotenv').config()
 let Discord = require("discord.js")
-
-
-
 let bot = new Discord.Client()
+
+let discordToken = process.env.DISCORD_TOKEN
 
 bot.on('ready', () => {
     console.log('Bot has started')
-    bot.user.setActivity("Fox", {
-        type: 'PLAYING'
-    })
-    updateName()
 })
 
 bot.on('error', e => {
@@ -17,18 +13,31 @@ bot.on('error', e => {
 })
 
 
-function updateName(){
+function updateName(nickname){
   bot.users.fetch("260956223132794881").then((res1) => {
     bot.guilds.fetch("726932810459512843").then((res) => {
-      res.member(res1).setNickname("foxup")
+      res.member(res1).setNickname(nickname)
     })
   })
 }
 
+function getName(){
+  return new Promise((resolve, reject) => {
+    bot.users.fetch("260956223132794881").then((res1) => {
+      bot.guilds.fetch("726932810459512843").then((res) => {
+        resolve(res.member(res1).displayName)
+      }).catch((err) => {
+        reject(err)
+      })
+    }).catch((err) => {
+      reject(err)
+    })
+  })
+}
 
-bot.login(token)
-
+bot.login(discordToken)
 
 module.exports = {
-  updateName
+  updateName,
+  getName
 }
