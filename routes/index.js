30d29d6a14decffa3cf7b3408de40d6c8d2687f7
Rishi Panthee <rrpanthe@ncsu.edu>
2020-09-02 13:04:29 -0500

    connect to discord

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 let express = require('express')
+let discord = require("../src/discord.js")
 
 let router = express.Router()
 
+
 router.post("/", (req, res) => {
-  
+
 })
 
 module.exports = router
